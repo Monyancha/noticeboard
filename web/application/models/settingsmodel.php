d17bac2ef62ae51e84f40bd1b0583ff776d5c83d
Salama A.B <devaksal@gmail.com>
2015-02-02 16:47:03 +0300

    Prep settings model

--- web/application/models/settingsmodel.php
+++ web/application/models/settingsmodel.php
@@ -14,8 +14,55 @@
 
 class SettingsModel extends CI_Model {
 
+    const TABLE = "settings";
+
     function __construct(){
         parent::__construct();
+    }
+
+
+    /**
+     * Get a given set of settings
+     * @param $name
+     * @return array
+     */
+    public function getSettings($name) {
+        $query = $this->db->get_where(self::TABLE, array('name' => $name));
+        $res = $query->result();
+        return json_decode($res['data']);
+    }
+
+    /**
+     * Set a set of configs
+     * @param $name
+     * @param $data
+     * @return mixed
+     */
+    public function setSettings($name, $data) {
+        $data = array(
+            'name' => $name ,
+            'data' => json_encode($data),
+        );
+        $this->db->insert(self::TABLE, $data);
+        return $this->db->insert_id();
+    }
+
+
+    /**
+     * Update settings
+     * @param $name
+     * @param $data
+     * @return mixed
+     */
+    public function updateSettings($name, $data) {
+        $data = array(
+            'data' => json_encode($data)
+        );
+
+        $this->db->where('name', $name);
+        $res = $this->db->update(self::TABLE, $data);
+
+        return $res; // TODO: Check Me!!
     }
 
 }
