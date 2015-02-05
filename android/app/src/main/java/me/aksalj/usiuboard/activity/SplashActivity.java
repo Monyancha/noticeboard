8b32eefb6f7d19e6689e5f721fa9cf684fe453f7
Salama A.B <devaksal@gmail.com>
2015-02-05 12:47:53 +0300

    Prep splash

---
+++ android/app/src/main/java/me/aksalj/usiuboard/activity/SplashActivity.java
@@ -0,0 +1,30 @@
+package me.aksalj.usiuboard.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import me.aksalj.usiuboard.R;
+
+/**
+ * Copyright (c) 2015 Salama AB
+ * All rights reserved
+ * Contact: aksalj@aksalj.me
+ * Website: http://www.aksalj.me
+ * <p/>
+ * Project : USIU Board
+ * File : me.aksalj.usiuboard.activity.SplashActivity
+ * Date : Feb, 05 2015 12:03 PM
+ * Description :
+ */
+public class SplashActivity extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // TODO: Register if first time load
+        // TODO: Load categories then start main activity
+
+    }
+}
