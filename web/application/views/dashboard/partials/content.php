5dd35ae03fe38bf8c54dfaaae6a7d86c61592840
Salama A.B <devaksal@gmail.com>
2015-02-27 20:47:53 +0300

    Prep content editor view

--- web/application/views/dashboard/partials/content.php
+++ web/application/views/dashboard/partials/content.php
@@ -1,3 +1,22 @@
+
 <div class="row">
     <p class="text-muted">Content editing will be done here.</p>
+
+    <div class="list-group col-md-4">
+        <?
+        $feeds = $this->FeedModel->getFeeds();
+        foreach ($feeds as $feed) {
+            echo "<a href='' class='list-group-item'>".$feed->title."</a>";
+        }
+        ?>
+    </div>
+
+    <div class="col-md-8">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <p>Click feed, display items here</p>
+            </div>
+        </div>
+    </div>
+
 </div>
