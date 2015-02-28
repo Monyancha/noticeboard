cc22e9a9a28c0ca8a05b9f46a3d2e05efe904ad6
Salama A.B <devaksal@gmail.com>
2015-02-28 18:51:55 +0300

    Update content edit UI; several UI fixes

---
+++ web/application/views/dashboard/partials/content_edit.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  Copyright (c) 2015 Salama AB
+ *  All rights reserved
+ *  Contact: aksalj@aksalj.me
+ *  Website: http://www.aksalj.me
+ *  
+ *  Project : web
+ *  File : content_edut.php
+ *  Date : 2/28/15 3:45 PM
+ *  Description :
+ *  
+ */
---
+++ web/application/views/dashboard/partials/urlRouter.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  Copyright (c) 2015 Salama AB
+ *  All rights reserved
+ *  Contact: aksalj@aksalj.me
+ *  Website: http://www.aksalj.me
+ *  
+ *  Project : web
+ *  File : urlRouter.php
+ *  Date : 2/28/15 5:28 PM
+ *  Description :
+ *  
+ */
